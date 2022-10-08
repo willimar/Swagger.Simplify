a33cc804c607b3485a0e03c82414f26e7bc3c3be
Willimar Rocha <willimar.rocha@auvo.com.br>
2022-10-08 19:14:38 -0300

    Lib para facilitar o uso do swagger versionando projetos em webapi

---
+++ Swagger.Simplify/Swagger.Simplify/ApiInfo.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swagger.Simplify.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swagger.Simplify
+{
+    public class ApiInfo
+    {
+        public int MajorVersion { get; set; } = 1;
+        public int MinorVersion { get; set; } = 1;
+        public string GroupNameFormat { get; set; } = $"'v'VVV";
+        public bool SubstituteApiVersionInUrl { get; set; } = true;
+        public bool AssumeDefaultVersionWhenUnspecified { get; set; } = false;
+        public bool ReportApiVersions { get; set; } = true;
+        public OpenApiInfo Info { get; set; } = new OpenApiInfo();
+        public OpenApiSecurityScheme SecurityScheme { get; set; } = ApiInfo.SecuritySchemeFactory();
+        public string KeyNameToXmlComents { get; set; } = string.Empty;
+        public Func<ApiDescription, IList<string>> TagActionBy { get; set; } = ApiInfo.GetTagActionBy();
+
+        public static ApiInfo Factory()
+        {
+            return new ApiInfo();
+        }
+
+        public virtual OpenApiSecurityRequirement SecurityRequirementFactory()
+        {
+            var result = new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = this.SecurityScheme.Scheme,
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            };
+
+            return result;
+        }
+
+        private static OpenApiSecurityScheme SecuritySchemeFactory()
+        {
+            var result = new OpenApiSecurityScheme()
+            {
+                Description = Resources.SecuritySchemeDescription,
+                Name = "Authorization",
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            };
+
+            return result;
+        }
+
+        private static Func<ApiDescription, IList<string>> GetTagActionBy()
+        {
+            return (api) =>
+            {
+                if (api.GroupName != null)
+                {
+                    return new[] { $"{api.GroupName}" };
+                }
+
+                if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+                {
+                    return new[] { controllerActionDescriptor.ControllerName };
+                }
+
+                throw new InvalidOperationException(Resources.GetTagActionException);
+            };
+        }
+    }
+}
---
+++ Swagger.Simplify/Swagger.Simplify/SwaggerSetup.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swagger.Simplify;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Builder;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Reflection;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System;
+using System.Linq;
+using System.Threading;
+using System.IO;
+using Swagger.Simplify.Properties;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class SwaggerSetup
+    {
+        public const string AllowAnyOrigins = "_AllowAnyOrigins";
+
+        public static void GenerateToVersion(this IServiceCollection services, ApiInfo apiInfo)
+        {
+            if (apiInfo is null)
+            {
+                throw new ArgumentNullException(nameof(apiInfo));
+            }
+
+            services.ConfigureVersions(apiInfo);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc($"v{apiInfo.MajorVersion}", apiInfo.Info);
+                c.AddSecurityDefinition(apiInfo.SecurityScheme.Scheme, apiInfo.SecurityScheme);
+                c.AddSecurityRequirement(apiInfo.SecurityRequirementFactory());
+                c.IncludeAllXmlComents(apiInfo.KeyNameToXmlComents);
+                c.TagActionsBy(apiInfo.TagActionBy);
+                c.DocInclusionPredicate((name, api) => true);
+                c.EnableAnnotations();
+            });
+            
+        }
+
+        public static void PrepareAnyCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(SwaggerSetup.AllowAnyOrigins,
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+        }
+
+        public static void ConfigureInSwaggerSimplify(this IApplicationBuilder app, CultureInfo cultureInfo, CultureInfo[] supportedCultures, Assembly assembly)
+        {
+            if (cultureInfo is null)
+            {
+                throw new ArgumentNullException(nameof(cultureInfo));
+            }
+
+            if (supportedCultures is null || !supportedCultures.Any())
+            {
+                supportedCultures = new CultureInfo[] { cultureInfo };
+            }
+
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(cultureInfo.Name),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures,
+            });
+
+            app.UseSwagger();
+
+            var provider = app.ApplicationServices.GetService<IApiVersionDescriptionProvider>() ?? throw new ArgumentNullException(Resources.apiVersionDescriptionProviderNotFound);
+            (AssemblyDescriptionAttribute descriptionAttribute, AssemblyProductAttribute productAttribute, AssemblyCopyrightAttribute copyrightAttribute, AssemblyName assemblyName) = SwaggerSetup.GetAssemblyInfo(assembly);
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint(
+                    $"/swagger/{description.GroupName}/swagger.json",
+                    $"{productAttribute?.Product} - {description.GroupName}");
+                }
+
+                options.DocExpansion(DocExpansion.List);
+            });
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseCors(SwaggerSetup.AllowAnyOrigins);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static (AssemblyDescriptionAttribute? descriptionAttribute, AssemblyProductAttribute? productAttribute, AssemblyCopyrightAttribute? copyrightAttribute, AssemblyName? assemblyName) GetAssemblyInfo(Assembly assembly)
+        {
+            var assemblyInfo = assembly.GetName();
+
+            var descriptionAttribute = assembly
+                 .GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)
+                 .OfType<AssemblyDescriptionAttribute>()
+                 .FirstOrDefault();
+            var productAttribute = assembly
+                 .GetCustomAttributes(typeof(AssemblyProductAttribute), false)
+                 .OfType<AssemblyProductAttribute>()
+                 .FirstOrDefault();
+            var copyrightAttribute = assembly
+                 .GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)
+                 .OfType<AssemblyCopyrightAttribute>()
+                 .FirstOrDefault();
+
+            return (descriptionAttribute, productAttribute, copyrightAttribute, assemblyInfo);
+        }
+
+        private static void IncludeAllXmlComents(this SwaggerGenOptions swaggerGenOptions, string keyName)
+        {
+            var pathToXmlDocumentsToLoad = AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(x => x.FullName != null && x.FullName.ToLower().Contains(keyName.ToLower()))
+                    .Select(x => Path.Combine(AppContext.BaseDirectory, $"{x.GetName().Name}.xml"))
+                    .Where(x => File.Exists(x))
+                    .ToList();
+
+            pathToXmlDocumentsToLoad.ForEach(doc => swaggerGenOptions.IncludeXmlComments(doc));
+        }
+
+        private static void ConfigureVersions(this IServiceCollection services, ApiInfo apiInfo)
+        {
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(apiInfo.MajorVersion, apiInfo.MinorVersion);
+                x.AssumeDefaultVersionWhenUnspecified = apiInfo.AssumeDefaultVersionWhenUnspecified;
+                x.ReportApiVersions = apiInfo.ReportApiVersions;
+            });
+
+            services.AddVersionedApiExplorer(p =>
+            {
+                p.GroupNameFormat = apiInfo.GroupNameFormat;
+                p.SubstituteApiVersionInUrl = apiInfo.SubstituteApiVersionInUrl;
+            });
+        }
+    }
+}
